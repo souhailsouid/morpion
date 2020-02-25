09218b37c9f3b288f4e2d53e84184c29b42ce470
Nattan Kifoyi <kifoyinattan@gmail.com>
2020-02-25 16:54:07 +0100

    Day 3

--- src/gameLogic/gameLogic.js
+++ src/gameLogic/gameLogic.js
@@ -1,20 +1,65 @@
 import { Cells, Board } from './boardLogic'
 
+export const GameStatus = {
+  DRAW: Symbol('draw'),
+  XVICTORY: Symbol('xVictory'),
+  OVICTORY: Symbol('oVictory'),
+  PLAYING: Symbol('playing')
+}
+Object.freeze(GameStatus)
+
 export class Game {
   constructor () {
     this.player = Cells.X
     this.board = new Board()
+    this.status = GameStatus.PLAYING 
+    this.count=0
   }
 
   getCurrentPlayer () {
     return this.player
   }
 
-  addPawn () {
+  addPawn (row, column) {
+    this.count++
+    if(this.count>=9){
+      return this.status = GameStatus.DRAW
+    }
+    this.board.addPawn(row,column, this.player)
+    this.checkStatus()
     this.player = this.player === Cells.O ? Cells.X : Cells.O
   }
 
-  getCell () {
-    return Cells.X
+  getCell (row, column) {
+    return this.board.getCell(row, column)
+  }
+
+  getStatus () {
+    return this.status
+  }
+
+  checkStatus () {
+
+    const potentialResult = this.player === Cells.X ? GameStatus.XVICTORY : GameStatus.OVICTORY
+    
+    for (let row = 0; row < 3; row++) {
+      if(this.getCell(row,0) === this.player && this.getCell(row,1) === this.player && this.getCell(row,2) === this.player){
+        this.status = potentialResult
+      }
+    } 
+
+    for (let col = 0; col < 3; col++) {
+      if(this.getCell(0,col) === this.player && this.getCell(1,col) === this.player && this.getCell(2,col) === this.player){
+        this.status = potentialResult
+      }
+    } 
+
+    if(this.getCell(0,0) === this.player && this.getCell(1,1) === this.player && this.getCell(2,2) === this.player){
+      this.status = potentialResult
+    }
+
+    if(this.getCell(0,2) === this.player && this.getCell(1,1) === this.player && this.getCell(2, 0) === this.player){
+      this.status = potentialResult
+    }
   }
 }
--- src/gameLogic/gameLogic.test.js
+++ src/gameLogic/gameLogic.test.js
@@ -1,4 +1,4 @@
-import { Game } from './gameLogic'
+import { Game, GameStatus } from './gameLogic'
 import { Cells } from './boardLogic'
 
 describe('Rules', () => {
@@ -31,10 +31,130 @@ describe('Rules', () => {
     expect(cell).toBe(Cells.X)
   })
 
-  it('adds a pawn that matches the current player', () => {
+  it('adds a second pawn that matches the current player', () => {
     game.addPawn(0, 0)
     game.addPawn(1, 0)
     const cell = game.getCell(1, 0)
     expect(cell).toBe(Cells.O)
+  })
+
+  it('game is not over ', () => {
+    const status = game.getStatus()
+    expect(status).toBe(GameStatus.PLAYING)
+  })
+
+  it('game is not over when we add 1 pawn', () => {
+    game.addPawn(1, 0)
+    const status = game.getStatus()
+    expect(status).toBe(GameStatus.PLAYING)
+  })
+
+  describe('game is over when 3 equal pawns are aligned', () => {
+
+
+    it('player X wins horizontaly on the first line', () => {
+      game.addPawn(0, 0)
+      game.addPawn(1, 1)
+      game.addPawn(0, 1)
+      game.addPawn(1, 0)
+      game.addPawn(0, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+
+    it('player X wins horizontaly on the second line', () => {
+      game.addPawn(1, 0)
+      game.addPawn(0, 1)
+      game.addPawn(1, 1)
+      game.addPawn(0, 0)
+      game.addPawn(1, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+
+    it('player X wins horizontaly on the third line', () => {
+      game.addPawn(2, 0)
+      game.addPawn(0, 1)
+      game.addPawn(2, 1)
+      game.addPawn(0, 0)
+      game.addPawn(2, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+
+    it('player O wins horizontaly on the first line', () => {
+      game.addPawn(1, 0)
+      game.addPawn(2, 0)
+      game.addPawn(0, 1)
+      game.addPawn(2, 1)
+      game.addPawn(0, 0)
+      game.addPawn(2, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.OVICTORY)
+    })
+
+    it('player X wins vertically on the first line', () => {
+      game.addPawn(0, 0)
+      game.addPawn(1, 1)
+      game.addPawn(1, 0)
+      game.addPawn(1, 2)
+      game.addPawn(2, 0)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+
+    it('player X wins diagonaly', () => {
+      game.addPawn(0, 0)
+      game.addPawn(0, 1)
+      game.addPawn(1, 1)
+      game.addPawn(0, 2)
+      game.addPawn(2, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+
+    it('player X wins second diagonaly', () => {
+      game.addPawn(0, 2)
+      game.addPawn(0, 1)
+      game.addPawn(1, 1)
+      game.addPawn(0, 2)
+      game.addPawn(2, 0)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.XVICTORY)
+    })
+  })
+
+  describe('game is not over when 3 different pawns are aligned', () => {
+
+    it('horizontaly', () => {
+      game.addPawn(1, 0)
+      game.addPawn(1, 1)
+      game.addPawn(0, 1)
+      game.addPawn(0, 0)
+      game.addPawn(1, 2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.PLAYING)
+    })
+    it('game is not over if we add a pawn ', () => {
+      game.addPawn(2,2)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.PLAYING)
+    })
+  })
+  describe('game over with a draw when the board is full', () => {
+    it('', () => {
+      game.addPawn(0, 0)
+      game.addPawn(0, 1)
+      game.addPawn(0, 2)
+      game.addPawn(1, 0)
+      game.addPawn(1, 1)
+      game.addPawn(2, 2)
+      game.addPawn(1, 2)
+      game.addPawn(2, 0)
+      game.addPawn(2, 1)
+      const status = game.getStatus()
+      expect(status).toBe(GameStatus.DRAW)
+    } )
+
   })
 })
